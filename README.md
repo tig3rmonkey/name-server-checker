e7dae693e8309570a616875b09436aa07eeb9d93
Kane Bright <voidptr@Kanes-MacBook-Pro.local>
2021-08-23 23:37:59 +1200

    Version 1.1 - Slight improvements. Automaticly outputs the results to a CSV file and groups the results alphabetically by their name servers. Formatting improvements etc

--- README.md
+++ README.md
@@ -6,17 +6,29 @@ Bash script that scans a list of domain names (provided in "domains.txt") for th
 * Checks for duplicate domain names supplied in the "domains.txt" file
 * Checks total number of domain names provided.
 * Checks whether the domain names are less than 4 characters long as they are likely invalid and/or spaces.
+* Results will also output to two files: tmp.csv (basic ns scan - no grouping) and grouped_ns_results.csv (this has the name servers ordered alphabetically, in effect grouping similar name servers)
+
+VERSION 1.1 IMPOVEMENTS:
+* Added numbering next to each domain/output
+* Better formatting/output for larger domain lists
+* Red/color text for warning messages
+* Results are now automatically outputed to a csv file and grouped alphabelically by name servers
 
 
 # USAGE:
 
+# Update the "domains.txt file with your list of domain names to check.
+# Each domain must be on its own line
+# Only the apex domain should be supplied. Supplying a domain with the prefixed "www" will simply not work.
+
 # Make the file executable
-chmod +x dnschecker.sh
+chmod +x nschecker.sh
 
 # Run script:
-./dnschecker.sh 
+./nschecker.sh 
 
-# Output result to a file:
-./dnschecker.sh > result.txt
+# Check stdout, tmp.csv, and grouped_ns_results.csv for the results.
+cat tmp.csv
+cat grouped_ns_results.csv
 
 
--- nschecker.sh
+++ nschecker.sh
@@ -22,41 +22,67 @@ _________ .__                   __
 
 EOF
 
+# Define Vars
 uniqcount=`uniq -d domains.txt | wc -l`
+intvar=1
+tmpcsv="tmp.csv"
+results="grouped_ns_results.csv"
 
 echo -n "Domains Being Queried: " && wc -l domains.txt | awk '{print $1}'
 echo -n "Duplicate Entries Found: "  && echo "$uniqcount" | awk '{print $1}'
 
 if [ "$uniqcount" -ge 1 ]; then
-	echo -e " * " `uniq -d domains.txt` 
+	echo -e "\033[41m [+] " `uniq -d domains.txt` "\033[m"
 fi
 
 echo -e '\n'
+cat /dev/null > $tmpcsv
 
 while read x; do
-	echo -n $x":"
-
-	if [ ${#x} -gt 15 ]; then
-		printf "\t\t"
+	echo -n "$intvar. $x : "
+	echo -n $x >> $tmpcsv       
+        
+	# length of maximum padding
+	#padding="......................................"
+	padding="                              "
+	
+	printf "%s %s ${padding:${#x}}"
+	printf "%s %s ${padding:${#x}}" >> $tmpcsv
+	
+	# Hack because numbering throws out formating when moving from single digits
+	if [ "${#intvar}" == 2 ]; then
+		echo -n "  "
+	elif [ "${#intvar}" == 3 ]; then
+		echo -n " "
+	
+	elif [ "${#intvar}" == 4 ]; then
+		echo -n ""
 	else
-		printf "\t\t\t"
+		echo -n "   "
 	fi
 	
 	if [ ${#x} -ge 4 ]; then	
-		#Sort the results before returning them to the screen
+		#Sort the results before returning them as they can return in any order
 		IFS=" " read -a myarray <<< `dig ns $x +short | xargs`
 		myarray=($(for each in ${myarray[@]}; do echo $each; done | sort))
 		echo ${myarray[@]}
+		echo ${myarray[@]} >> $tmpcsv
+	
 	else
-		echo -e "-\t!! WARNING: THE DOMAIN IS LESS THAN 4 CHARACTERS LONG - PLEASE CHECK THE \"domains.txt\" FILE"
+		echo -e "\033[41m!! WARNING: THE DOMAIN IS LESS THAN 4 CHARACTERS LONG - PLEASE CHECK THE \"domains.txt\" FILE\033[m"
+		echo -e "!! WARNING: THE DOMAIN IS LESS THAN 4 CHARACTERS LONG - PLEASE CHECK THE \"domains.txt\" FILE" >> $tmpcsv
 	fi
 	
+	intvar=$((intvar+1))
 	#You MAY need to enable sleep if the results become a little skewed. 	
 	#sleep 1
 done < domains.txt
+
+sort -b -k 2 $tmpcsv > $results
 
 # Lazy formatting
 cat << "EOF"
 
 
 EOF
+
